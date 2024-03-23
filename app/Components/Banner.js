75dd6815b9a6d99d68f03a8b30f54baa8b9d759a
tmg-m <bus.mahendrat@gmail.com>
2024-03-24 00:47:12 +0100

    banner component

---
+++ app/Components/Banner.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Banner() {
+  return (
+    <div className="flex items-center">
+      <Image
+          src="https://fastly.picsum.photos/id/269/1000/500.jpg?hmac=Es_1r5dB5ny8OfpgyDNCXMZwYmhG6KLtfYrP6-kmg4E"
+          alt="iphone 15 pro max"
+          width={500}
+          height={200}
+        />
+      <p>Banner for products</p>
+    </div>
+  );
+}
