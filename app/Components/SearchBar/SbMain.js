857d2c3db6b29f688c766d4c8af463ec4037f912
tmg-m <bus.mahendrat@gmail.com>
2024-03-24 17:12:24 +0100

    search Bar implementation

---
+++ app/Components/SearchBar/SbMain.js
@@ -0,0 +1,23 @@
+import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import LocalShippingOutlinedIcon from '@mui/icons-material/LocalShippingOutlined';
+export default function SbMain() {
+  return (
+    <div className="flex justify-between items-center mx-5 mb-8 py-5">
+      <p className="">Logo</p>
+      <div className="flex justify-between">
+        
+        <a href='https://www.google.com' className="mx-5">Home</a>
+        <a href='https://www.google.com' className="mx-5">Phone</a>
+        <a href='https://www.google.com' className="mx-5">Tablets</a>
+        <a href='https://www.google.com' className="mx-5">TV</a>
+        <a href='https://www.google.com' className="mx-5">Accessories</a>
+      </div>
+      <div className="flex justify-between">
+        <LocalShippingOutlinedIcon className='mr-8' />
+        <ShoppingBagOutlinedIcon className='mr-8' />
+        <AccountCircleOutlinedIcon className='' />
+      </div>
+    </div>
+  );
+}
--- app/Pages/Home.js
+++ app/Pages/Home.js
@@ -1,12 +1,16 @@
 import Banner from "../Components/Banner";
 import Card from "../Components/Card";
+import SbMain from "../Components/SearchBar/SbMain";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center"> 
-      <p class="mb-8">Next js test home</p>
-      <Card/>
-      <Banner/>
+    <div className="flex flex-col justify-center"> 
+      <SbMain />
+      <div class="mx-5">
+        <p class="mb-8">Next js test home</p>
+        <Card/>
+        <Banner/>
+      </div>
     </div>
   );
 }
