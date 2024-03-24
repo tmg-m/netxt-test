c70ac10009c6df30dbf0464daa8dd97fcb2cd87e
tmg-m <bus.mahendrat@gmail.com>
2024-03-24 14:31:13 +0100

    add CtaBtn component

---
+++ app/Components/Button/CtaBtn.js
@@ -0,0 +1,7 @@
+export default function CtaBtn() {
+  return (
+    <div className="flex items-center justify-center px-8 py-3 bg-slate-600 rounded-full cursor-pointer">
+        <a href="https://www.google.com" >Click here</a>
+    </div>
+  );
+}
