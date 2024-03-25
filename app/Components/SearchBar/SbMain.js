a828c6e185c6d18c43db9ceb5d971ba838fa758c
tmg-m <bus.mahendrat@gmail.com>
2024-03-25 19:11:13 +0100

    routs and sb test

--- app/Components/SearchBar/SbMain.js
+++ app/Components/SearchBar/SbMain.js
@@ -4,16 +4,16 @@ import LocalShippingOutlinedIcon from '@mui/icons-material/LocalShippingOutlined
 export default function SbMain() {
   return (
     <div className="flex justify-between items-center mx-5 mb-8 py-5">
-      <p className="">Logo</p>
+      <a href='/' className="">Logo</a>
       <div className="flex justify-between">
         
-        <a href='https://www.google.com' className="mx-5">Home</a>
-        <a href='https://www.google.com' className="mx-5">All products</a>
-        <a href='https://www.google.com' className="mx-5">Phone</a>
-        <a href='https://www.google.com' className="mx-5">Tablet</a>
-        <a href='https://www.google.com' className="mx-5">TV</a>
-        <a href='https://www.google.com' className="mx-5">Accessories</a>
-        <a href='https://www.google.com' className="mx-5">About</a>
+        <a href='/' className="mx-5">Home</a>
+        <a href='/products' className="mx-5">All products</a>
+        <a href='/products/phone' className="mx-5">Phone</a>
+        <a href='/products/tablets' className="mx-5">Tablet</a>
+        <a href='/products/tv' className="mx-5">TV</a>
+        <a href='/products/accessories' className="mx-5">Accessories</a>
+        <a href='/about' className="mx-5">About</a>
       </div>
       <div className="flex justify-between">
         <LocalShippingOutlinedIcon className='mr-8' />
--- app/page.js
+++ app/page.js
@@ -6,7 +6,32 @@ export default function Home() {
     <div className="flex flex-col justify-center"> 
       <div class="mx-5">
         <p class="mb-8">My Home page</p>
-        <Card/>
+        <div className="flex gap-10 justify-center">
+          <Card
+            id={1}
+            imageUrl={"https://fastly.picsum.photos/id/2/200/200.jpg?hmac=isSWZUpv7D1D156XcADPOCZlfCG9mmvb8OlXFdvLdK0"}
+            title={'iphone 15 pro max'}
+            description={'This is iphone 15 pro max with amazing camera.'}
+          />
+          <Card
+            id={1}
+            imageUrl={"https://fastly.picsum.photos/id/2/200/200.jpg?hmac=isSWZUpv7D1D156XcADPOCZlfCG9mmvb8OlXFdvLdK0"}
+            title={'iphone 15 pro max'}
+            description={'This is iphone 15 pro max with amazing camera.'}
+          />
+          <Card
+            id={1}
+            imageUrl={"https://fastly.picsum.photos/id/2/200/200.jpg?hmac=isSWZUpv7D1D156XcADPOCZlfCG9mmvb8OlXFdvLdK0"}
+            title={'iphone 15 pro max'}
+            description={'This is iphone 15 pro max with amazing camera.'}
+          />
+          <Card
+            id={1}
+            imageUrl={"https://fastly.picsum.photos/id/2/200/200.jpg?hmac=isSWZUpv7D1D156XcADPOCZlfCG9mmvb8OlXFdvLdK0"}
+            title={'iphone 15 pro max'}
+            description={'This is iphone 15 pro max with amazing camera.'}
+          />
+        </div>
         <Banner/>
       </div>
     </div>
