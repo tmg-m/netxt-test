afc36dccdde4058a0c6e91a94c1dd0159da9a28f
tmg-m <bus.mahendrat@gmail.com>
2024-03-26 23:30:48 +0100

    card css unfinished

--- app/Components/Card.js
+++ app/Components/Card.js
@@ -1,18 +1,18 @@
 import Image from "next/image";
 import CtaBtn from "./Button/CtaBtn";
-import ShoppingBasketRoundedIcon from '@mui/icons-material/ShoppingBasketRounded';
+import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
 
 export default function Card({ title, description, imageUrl, id }) {
   return (
-    <div className="relative flex flex-col justify-center items-center mb-8 w-[300px] p-10" style={{ backgroundColor: 'red' }}>
-      <ShoppingBasketRoundedIcon className="absolute top-2 right-2 p-1 text-3xl" style={{ color: 'white' }} />
-      <div className="mb-2 flex items-center justify-center max-w-full w-100 h-[300px]">
+    <div className="shadow-2xl relative flex flex-col justify-center items-center mb-8 w-[300px] p-10 rounded-xl">
+      <ShoppingBagOutlinedIcon className="absolute top-2 right-2 p-1 text-3xl"/>
+      <div className="mb-2 flex items-center justify-center max-w-full w-100 h-[300px] rounded-md overflow-hidden">
         <Image
-          className="w-100 h-100"
+          className="w-full h-full object-cover"
           src={imageUrl}
           alt="iphone 15 pro max"
-          width={200}
-          height={200}
+          height={100}
+          width={100}
         />
       </div>
       <div className="mb-2">
