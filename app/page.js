f56c684fcad6c06356fe77e8c6de04041f3edf57
tmg-m <bus.mahendrat@gmail.com>
2024-03-27 21:18:29 +0100

    hero added for test

--- app/page.js
+++ app/page.js
@@ -1,12 +1,19 @@
 import Banner from "./Components/Banner";
 import Card from "./Components/Card";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center"> 
-      <div class="mx-5">
-        <p class="mb-8">My Home page</p>
-        <div className="flex gap-10 justify-center">
+    <div className="pt-10"> 
+      <div class="flex flex-col w-full">
+        <div className="flex justify-center items-center py-10 md:py-20 w-100">
+          <Image src="/heroImg.jpg"
+            alt="iphone 15 pro max"
+            height={1000}
+            width={1000}
+          />
+        </div>
+        <div className="flex gap-10 justify-center items-center w-full bg-ribbon py-10">
           <Card
             id={1}
             imageUrl="/iphone15test.png"
