0d60c0bbc9dc570012414e2effc068654b4e706c
tmg-m <bus.mahendrat@gmail.com>
2024-03-27 19:10:34 +0100

    card style test with local image

--- app/Components/Button/CtaBtn.js
+++ app/Components/Button/CtaBtn.js
@@ -1,7 +1,9 @@
+import Link from "next/link";
+
 export default function CtaBtn({id}) {
   return (
     <div className="flex items-center justify-center px-8 py-3 bg-slate-600 rounded-full cursor-pointer">
-        <a href={`/products/${id}`}>Click here</a>
+        <Link href={`/products/${id}`}>Click here</Link>
     </div>
   );
 }
--- app/page.js
+++ app/page.js
@@ -9,25 +9,25 @@ export default function Home() {
         <div className="flex gap-10 justify-center">
           <Card
             id={1}
-            imageUrl={"https://fastly.picsum.photos/id/2/200/200.jpg?hmac=isSWZUpv7D1D156XcADPOCZlfCG9mmvb8OlXFdvLdK0"}
+            imageUrl="/iphone15test.png"
             title={'iphone 15 pro max'}
             description={'This is iphone 15 pro max with amazing camera.'}
           />
           <Card
             id={1}
-            imageUrl={"https://fastly.picsum.photos/id/2/200/200.jpg?hmac=isSWZUpv7D1D156XcADPOCZlfCG9mmvb8OlXFdvLdK0"}
+            imageUrl="/iphone15test.png"
             title={'iphone 15 pro max'}
             description={'This is iphone 15 pro max with amazing camera.'}
           />
           <Card
             id={1}
-            imageUrl={"https://fastly.picsum.photos/id/2/200/200.jpg?hmac=isSWZUpv7D1D156XcADPOCZlfCG9mmvb8OlXFdvLdK0"}
+            imageUrl="/iphone15test.png"
             title={'iphone 15 pro max'}
             description={'This is iphone 15 pro max with amazing camera.'}
           />
           <Card
             id={1}
-            imageUrl={"https://fastly.picsum.photos/id/2/200/200.jpg?hmac=isSWZUpv7D1D156XcADPOCZlfCG9mmvb8OlXFdvLdK0"}
+            imageUrl="/iphone15test.png"
             title={'iphone 15 pro max'}
             description={'This is iphone 15 pro max with amazing camera.'}
           />
