8892ec3221cac9303cc29a5099d19c06f915886c
tmg-m <bus.mahendrat@gmail.com>
2024-03-28 14:30:31 +0100

    GallaryCycle banner Created

---
+++ app/Components/Gallery/GalleryCycle.js
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+export default function GalleryCycle() {
+  const ImagesURL = [
+    "https://fastly.picsum.photos/id/121/800/500.jpg?hmac=gLwxO0vi9Z0866UIQuisCQ2hU_EcFDOwDPcHaraYzgs",
+    "https://fastly.picsum.photos/id/197/800/500.jpg?hmac=_1FsJF7nVOh_947d0rS9fXa-_cMGn7WyK0D2GRlv9Mo",
+    "https://fastly.picsum.photos/id/77/800/500.jpg?hmac=t0e6z0Dz5ikZyOHngyNIjZv9l2PbT0DM1UVwFLFvSeQ",
+    "https://fastly.picsum.photos/id/193/800/500.jpg?hmac=BGVYvUv0Zp3x25HD6lFCMR8VhOWE6YitVnV0in4WvTk",
+    "https://fastly.picsum.photos/id/219/800/500.jpg?hmac=XNeOJOXGPiaifsmBtcVYMDutANQJirvAhFgXmeYTs2Y",
+    "https://fastly.picsum.photos/id/59/800/500.jpg?hmac=8a-15mf0CC224f-fb6ZVmwCQ21iKL9K_YVWFW3HtysA",
+    "https://fastly.picsum.photos/id/103/800/500.jpg?hmac=KZ6dkXxlup8OONCF8sDQcAQt2zo9B6FNf5CBywHU8Rg",
+    "https://fastly.picsum.photos/id/144/800/500.jpg?hmac=yezG8mHf_22wI-UZ4ah6UkzpaZfZ3DIhoIT4nUTg9ZI",
+    "https://fastly.picsum.photos/id/1002/800/500.jpg?hmac=HB2dsrXHS8TFUYRMgPfd0qZZKqxA5EBsibOmmd8iU4o",
+    "https://fastly.picsum.photos/id/656/800/500.jpg?hmac=VsO7TVUR4hlp5-gvjA0yPvDIbVJl4RdRWhhYyj8vGuY"
+  ];
+
+  return (
+    <div className="flex items-center w-100 overflow-hidden">
+      {ImagesURL.map((url, index) => (
+        <Image className="" key={index} src={url} alt={`Image ${index + 1}`} width={400} height={400} />
+      ))}
+    </div>
+  );
+}
