fe7ce98bb4b447b6887fa4c9b6c0008aff1267b3
tmg-m <bus.mahendrat@gmail.com>
2024-03-30 00:32:15 +0100

    responsive Nav added

--- app/Components/NavBar/MainNav.js
+++ app/Components/NavBar/MainNav.js
@@ -1,24 +1,48 @@
+"use client"
+
+import React, { useState, useEffect } from 'react';
 import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import LocalShippingOutlinedIcon from '@mui/icons-material/LocalShippingOutlined';
-export default function SbMain() {
+import MainNavResponsive from './MainNavResponsive';
+
+export default function MainNav() {
+  const [isMobile, setIsMobile] = useState(false)
+
+  useEffect(() => {
+    const updateIsMobile = () => {
+      setIsMobile(window.innerWidth < 768)
+    };
+    updateIsMobile();
+    window.addEventListener('resize', updateIsMobile)
+    return () => {
+      window.removeEventListener('resize', updateIsMobile)
+    };
+  }, []);
+
+  console.log(isMobile)
   return (
-    <div className="flex justify-between items-center mx-5 py-5">
-      <a href='/' className="">Logo</a>
-      <div className="flex justify-between items-center">
-        <a href='/' className="mx-5">Home</a>
-        <a href='/products' className="mx-5">All products</a>
-        <a href='/products/phone' className="mx-5">Phone</a>
-        <a href='/products/tablets' className="mx-5">Tablet</a>
-        <a href='/products/tv' className="mx-5">TV</a>
-        <a href='/products/accessories' className="mx-5">Accessories</a>
-        <a href='/about' className="mx-5">About</a>
-      </div>
-      <div className="flex justify-between text-gray-500">
-        <LocalShippingOutlinedIcon className='mr-8' />
-        <ShoppingBagOutlinedIcon className='mr-8' />
-        <AccountCircleOutlinedIcon className='' />
-      </div>
-    </div>
+    <>
+      {isMobile ? 
+        <MainNavResponsive /> :
+        <div className="flex justify-between items-center mx-5 py-5">
+          <a href='/' className="">Logo</a>
+          <div className="flex justify-between items-center">
+            <a href='/' className="mx-5">Home</a>
+            <a href='/products' className="mx-5">All products</a>
+            <a href='/products/phone' className="mx-5">Phone</a>
+            <a href='/products/tablets' className="mx-5">Tablet</a>
+            <a href='/products/tv' className="mx-5">TV</a>
+            <a href='/products/accessories' className="mx-5">Accessories</a>
+            <a href='/about' className="mx-5">About</a>
+          </div>
+          <div className="flex justify-between text-gray-500">
+            <LocalShippingOutlinedIcon className='mr-8' />
+            <ShoppingBagOutlinedIcon className='mr-8' />
+            <AccountCircleOutlinedIcon className='' />
+          </div>
+        </div>
+      }
+    </>
   );
 }
--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
 import { Inter } from "next/font/google";
 import "./tailwind.css";
 import MainNav from "./Components/NavBar/MainNav"
-import MainNavResponsive from "./Components/NavBar/MainNavResponsive"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +15,6 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <div className="border-b fixed w-full backdrop-blur-md z-50" >
           <MainNav />
-          <MainNavResponsive/>
         </div>
         <div className="pt-20">
           {children}
