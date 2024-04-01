d2faabbdcec11cb6270738a5ad656466e05f8273
tmg-m <bus.mahendrat@gmail.com>
2024-04-02 01:51:43 +0200

    cta btn changes

--- app/Components/Button/CtaBtn.js
+++ app/Components/Button/CtaBtn.js
@@ -1,9 +1,9 @@
 import Link from "next/link";
 
-export default function CtaBtn({id}) {
+export default function CtaBtn({id, typeBtn}) {
   return (
-    <div className="flex items-center justify-center px-8 py-3 bg-slate-600 rounded-full cursor-pointer">
-        <Link href={`/products/${id}`}>Click here</Link>
+    <div className={`flex items-center justify-center px-8 py-3 text-white rounded-3xl cursor-pointer ${typeBtn === 'checkout' ?  'bg-red-600' : 'bg-black'}`}>
+        <Link href={`/products/&${typeBtn}`}>{typeBtn === 'checkout' ?  'Buy' : 'Add to Cart'}</Link>
     </div>
   );
 }
