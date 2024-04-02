ab842a91e8047b7e32c360899410be49b63a704b
tmg-m <bus.mahendrat@gmail.com>
2024-04-03 01:51:39 +0200

    summery page added to nav with link updates

--- app/Components/NavBar/MainNav.js
+++ app/Components/NavBar/MainNav.js
@@ -4,6 +4,7 @@ import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import LocalShippingOutlinedIcon from '@mui/icons-material/LocalShippingOutlined';
 import MainNavResponsive from './MainNavResponsive';
+import Link from 'next/link';
 
 export default function MainNav() {
   const [isMobile, setIsMobile] = useState(false)
@@ -21,20 +22,20 @@ export default function MainNav() {
       {isMobile ? 
         <MainNavResponsive /> :
         <div className="flex justify-between items-center mx-5 py-5">
-          <a href='/' className="">Logo</a>
+          <Link href='/' className="">Logo</Link>
           <div className="flex justify-between items-center">
-            <a href='/' className="mx-5">Home</a>
-            <a href='/products' className="mx-5">All products</a>
-            <a href='/products/phone' className="mx-5">Phone</a>
-            <a href='/products/tablets' className="mx-5">Tablet</a>
-            <a href='/products/tv' className="mx-5">TV</a>
-            <a href='/products/accessories' className="mx-5">Accessories</a>
-            <a href='/about' className="mx-5">About</a>
+            <Link href='/' className="mx-5">Home</Link>
+            <Link href='/products' className="mx-5">All products</Link>
+            <Link href='/products/phone' className="mx-5">Phone</Link>
+            <Link href='/products/tablets' className="mx-5">Tablet</Link>
+            <Link href='/products/tv' className="mx-5">TV</Link>
+            <Link href='/products/accessories' className="mx-5">Accessories</Link>
+            <Link href='/about' className="mx-5">About</Link>
           </div>
-          <div className="flex justify-between text-gray-500">
-            <LocalShippingOutlinedIcon className='mr-8' />
-            <ShoppingBagOutlinedIcon className='mr-8' />
-            <AccountCircleOutlinedIcon className='' />
+          <div className="flex gap-5 text-gray-500">
+            <Link href="#"><LocalShippingOutlinedIcon href="#" className='' /></Link>
+            <Link href="/summary"><ShoppingBagOutlinedIcon className='' /></Link>
+            <Link href="#"><AccountCircleOutlinedIcon href="" className='' /></Link>
           </div>
         </div>
       }
