4fb9899b79f50d1b0c9cb71b6925c6b5425ff578
tmg-m <bus.mahendrat@gmail.com>
2024-04-02 03:05:13 +0200

    component GallaryProduct added to product page

--- app/products/[productType]/[...productId]/page.js
+++ app/products/[productType]/[...productId]/page.js
@@ -1,45 +1,19 @@
 import CtaBtn from "@/app/Components/Button/CtaBtn";
+import GalleryProduct from "@/app/Components/Gallery/GalleryProduct";
 import Image from "next/image";
 
 export default function ProductId({ params }) {
   /* const { productType, productId } = params; */
+  const galleryImg = [
+    '/samsungPhone1.png',
+    '/samsungPhone1.png',
+    '/samsungPhone1.png'
+  ]
 
   return (
     <div className="flex flex-col justify-center items-center">
-      <div className="flex flex-col md:flex-row bg-ribbon w-full justify-center p-10 gap-10 md:gap-20">
-        <div className="flex justify-center items-center">
-          <div className="flex flex-col gap-3 md:gap-5 md:flex-row rounded-3xl overflow-hidden shadow-2xl w-full">
-            <div className="flex items-center justify-center md:w-[60%] bg-white shadow-2xl">
-              <Image
-                src={"/samsungPhone1.png"}
-                className="object-contain bg-white"
-                alt={`image`}
-                height={400}
-                width={400}
-              />
-            </div>
-            <div className="flex justify-between md:grid md:grid-cols-1 gap-3 md:gap-5 md:grid-rows-2 md:flex-col md:w-[40%]">
-              <div className="flex items-center justify-center shadow-2xl">
-                <Image
-                  src={"/samsungPhone1.png"}
-                  className="object-contain bg-white"
-                  alt={`image`}
-                  height={400}
-                  width={400}
-                />
-              </div>
-              <div className="flex items-center justify-center bg-white shadow-2xl">
-                <Image
-                  src={"/samsungPhone1.png"}
-                  className="object-contain bg-white"
-                  alt={`image`}
-                  height={400}
-                  width={400}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
+      <div className="flex flex-col md:flex-row bg-ribbon w-full justify-center p-10 md:p-20 gap-10 md:gap-20">
+        <GalleryProduct galleryImg={galleryImg}/>
 
         <div className="flex flex-col justify-between md:min-w-[300px] md:max-w-[400px]">
           <div className="flex flex-col justify-between">
@@ -69,9 +43,11 @@ export default function ProductId({ params }) {
           </div>
         </div>
       </div>
-      <div className="w-full p-20">
-        <div>
-          <p>discription</p>
+      <div className="w-full p-10 md:p-20">
+        <div className="flex flex-col gap-5">
+          <p>Title brand</p>
+          <p>Title phone</p>
+          <p>discription asdfajsdf asdfl ajsd asdklfjasd asdklf asdlfas dlfka jsdlfkjasd kl jasdkl jf asdfasd lfkasdhj kasdf lasdkf jsdkla jlasdk jflsdka jklfa jkl jklas jldkajlk asdkl jklasd ldk f</p>
         </div>
       </div>
     </div>
