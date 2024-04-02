67366b4a0f1d317c21ef6c8f39f123fc212d0039
tmg-m <bus.mahendrat@gmail.com>
2024-04-02 22:57:17 +0200

    ctaBtn configured for checkout

--- app/Components/Button/CtaBtn.js
+++ app/Components/Button/CtaBtn.js
@@ -1,9 +1,27 @@
-import Link from "next/link";
+"use client";
+export default function CtaBtn({ id, type, typeBtn, isEnable }) {
+  const handleClick = () => {
+    const params = {
+      id: id,
+      type: type,
+    };
+    const queryString = new URLSearchParams(params).toString();
+    if (isEnable) {
+      typeBtn == "checkout"
+        ? (window.location.href = `/checkout?${queryString}`)
+        : console.log("here");
+    }
+    console.log(isEnable);
+  };
 
-export default function CtaBtn({id, typeBtn}) {
   return (
-    <div className={`flex items-center justify-center px-8 py-3 text-white rounded-3xl cursor-pointer ${typeBtn === 'checkout' ?  'bg-red-600' : 'bg-black'}`}>
-        <Link href={`/products/&${typeBtn}`}>{typeBtn === 'checkout' ?  'Buy' : 'Add to Cart'}</Link>
+    <div
+      className={`flex items-  justify-center px-8 py-3 text-white rounded-3xl cursor-pointer 
+      ${!isEnable ? "bg-red-300" : ""}
+      ${typeBtn === "checkout" ? "bg-red-600" : "bg-black"}`}
+      onClick={handleClick}
+    >
+      {typeBtn === "checkout" ? "Buy" : "Add to Cart"}
     </div>
   );
 }
