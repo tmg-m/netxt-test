9093be82f6676620f2c8b4f387a14253670b0b4d
tmg-m <bus.mahendrat@gmail.com>
2024-04-04 11:38:24 +0200

    Get routes for all products

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { products } from  "../../data.json"
+import { products } from  "@/data.json"
 
 export async function GET() {
   return NextResponse.json(
