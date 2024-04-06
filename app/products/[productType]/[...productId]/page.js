31672891079efdc3dc863196ec953b6f6fe14efe
tmg-m <bus.mahendrat@gmail.com>
2024-04-06 05:22:14 +0200

    massive changes on productIdPage with isLoaded,selectStorate and its configs with cards updates and function to selectStorage

--- app/products/[productType]/[...productId]/page.js
+++ app/products/[productType]/[...productId]/page.js
@@ -1,54 +1,94 @@
+"use client";
 import CtaBtn from "@/app/Components/Button/CtaBtn";
 import GalleryProduct from "@/app/Components/Gallery/GalleryProduct";
+import { useEffect, useState } from "react";
 
 export default function ProductId({ params }) {
-  /* const { productType, productId } = params; */
-  const galleryImg = [
-    '/samsungPhone1.png',
-    '/samsungPhone1.png',
-    '/samsungPhone1.png'
-  ]
+  const [isLoaded, setIsLoaded] = useState(false);
+  const { productType, productId } = params;
+  const [product, setProduct] = useState(null);
+  const [selectStorage, setSelectStorage] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`/api/products/${productType}/${productId}`);
+        if (response.ok) {
+          const [data] = await response.json();
+          setProduct(data);
+          setIsLoaded(true);
+        }
+      } catch (error) {
+        console.error("Error fetching product:", error);
+      }
+    };
+    fetchData();
+  }, [productType, productId]);
+  
+
+  useEffect(() => {
+    if (product && isLoaded && product.storage_options) {
+      const firstStorageOption = product.storage_options[0];
+      setSelectStorage(firstStorageOption);
+    }
+  }, [product, isLoaded]);
+
+  const handleSizeChange = ({size, price}) => {
+    setSelectStorage({'size': size , 'price': price})
+  }
 
   return (
-    <div className="flex flex-col justify-center items-center">
-      <div className="flex flex-col md:flex-row bg-ribbon w-full justify-center p-10 md:p-20 gap-10 md:gap-20">
-        <GalleryProduct galleryImg={galleryImg}/>
+    isLoaded && (
+      <div className="flex flex-col justify-center items-center">
+        <div className="flex flex-col md:flex-row bg-ribbon w-full justify-center p-10 md:p-20 gap-10 md:gap-20">
+          <GalleryProduct galleryImg={product.image_url} />
 
-        <div className="flex flex-col justify-between md:min-w-[300px] md:max-w-[400px]">
-          <div className="flex flex-col justify-between">
-            <div className="flex flex-col gap-5">
-              <p>Apple</p>
-              <p className="text-4xl">Iphone 15 pro max asdfasdf</p>
-              <p className="text-2xl">$1500</p>
+          <div className="flex flex-col justify-between md:min-w-[300px] md:max-w-[400px]">
+            <div className="flex flex-col justify-between">
+              <div className="flex flex-col gap-5">
+                <p>{product?.type}</p>
+                <p className="text-4xl">{product?.title}</p>
+                <p className="text-2xl">
+                  {product.currency}{selectStorage?.price}
+                </p>
+              </div>
+              <div className="flex flex-col mt-10 gap-3">
+                {product.storage_options &&
+                  product.storage_options.map(({ size, price }) => (
+                    <div
+                      key={size}
+                      className="flex p-5 justify-between items-center bg-white shadow-md rounded-xl cursor-pointer"
+                      onClick={() => handleSizeChange({size, price})}
+                    >
+                      <p>{size}</p>
+                      <p>{product.currency}{price}</p>
+                    </div>
+                  ))}
+              </div>
             </div>
-            <div className="flex flex-col mt-10 gap-3">
-              <div className="flex p-5 justify-between items-center bg-white shadow-md rounded-xl cursor-pointer">
-                <p>128 GB</p>
-                <p>$1000</p>
-              </div>
-              <div className="flex p-5 justify-between items-center bg-white shadow-md rounded-xl cursor-pointer">
-                <p>256 GB</p>
-                <p>$1200</p>
-              </div>
-              <div className="flex p-5 justify-between items-center bg-white shadow-md rounded-xl cursor-pointer">
-                <p>512 GB</p>
-                <p>$1500</p>
-              </div>
+            <div className="flex flex-col mt-10 gap-5">
+              <CtaBtn
+                id={productId}
+                typeBtn={"checkout"}
+                type={productType}
+                isEnable
+              />
+              <CtaBtn
+                id={productId}
+                typeBtn={"basket"}
+                type={productType}
+                isEnable
+              />
             </div>
           </div>
-          <div className="flex flex-col mt-10 gap-5">
-            <CtaBtn id={1} typeBtn={"checkout"} type={'phone'} isEnable/>
-            <CtaBtn id={1} typeBtn={"basket"} type={'phone'} isEnable/>
+        </div>
+        <div className="w-full p-10 md:p-20">
+          <div className="flex flex-col gap-5">
+            <p className="text-3xl">{product?.title}</p>
+            <p>{product?.description}</p>
           </div>
         </div>
       </div>
-      <div className="w-full p-10 md:p-20">
-        <div className="flex flex-col gap-5">
-          <p>Title brand</p>
-          <p>Title phone</p>
-          <p>discription asdfajsdf asdfl ajsd asdklfjasd asdklf asdlfas dlfka jsdlfkjasd kl jasdkl jf asdfasd lfkasdhj kasdf lasdkf jsdkla jlasdk jflsdka jklfa jkl jklas jldkajlk asdkl jklasd ldk f</p>
-        </div>
-      </div>
-    </div>
+    )
   );
 }
