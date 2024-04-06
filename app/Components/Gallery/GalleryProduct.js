dd50551c4a4780f78b7eb96689e75ee1c3af2611
tmg-m <bus.mahendrat@gmail.com>
2024-04-06 05:19:55 +0200

    productId galler changes

--- app/Components/Gallery/GalleryProduct.js
+++ app/Components/Gallery/GalleryProduct.js
@@ -1,15 +1,17 @@
+"use client"
 import Image from "next/image";
 
 export default function GalleryProduct({ galleryImg }) {
+  
   return (
     <>
       {galleryImg && (
         <div className="flex justify-center items-center">
           <div className="flex flex-col gap-3 md:gap-5 md:flex-row rounded-3xl overflow-hidden shadow-2xl w-full">
-            {galleryImg[0] && (
-              <div className="flex items-center justify-center md:w-[60%] bg-white shadow-2xl">
+            {galleryImg && (
+              <div className="flex items-center justify-center md:w-[60%] bg-white shadow-2xl p-3">
                 <Image
-                  src={galleryImg[0]}
+                  src={galleryImg.main}
                   className="object-contain"
                   alt={`image`}
                   height={400}
@@ -18,10 +20,10 @@ export default function GalleryProduct({ galleryImg }) {
               </div>
             )}
             <div className="flex justify-between md:grid md:grid-cols-1 gap-3 md:gap-5 md:grid-rows-2 md:flex-col md:w-[40%]">
-              {galleryImg[1] && (
-                <div className="flex items-center justify-center shadow-2xl bg-white">
+              {galleryImg.sub.primary && (
+                <div className="flex items-center justify-center shadow-2xl bg-white p-3">
                   <Image
-                    src={galleryImg[1]}
+                    src={galleryImg.sub.primary}
                     className="object-contain"
                     alt={`image`}
                     height={400}
@@ -29,10 +31,10 @@ export default function GalleryProduct({ galleryImg }) {
                   />
                 </div>
               )}
-              {galleryImg[2] && (
-                <div className="flex items-center justify-center bg-white shadow-2xl">
+              {galleryImg.sub.secondary && (
+                <div className="flex items-center justify-center bg-white shadow-2xl p-3">
                   <Image
-                    src={galleryImg[2]}
+                    src={galleryImg.sub.secondary}
                     className="object-contain"
                     alt={`image`}
                     height={400}
