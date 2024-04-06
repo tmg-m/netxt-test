1d2a288792962bc09999ddada720e6d7f6b4f219
tmg-m <bus.mahendrat@gmail.com>
2024-04-06 05:20:40 +0200

    context data added with its configs

--- app/context/index.js
+++ app/context/index.js
@@ -1,15 +1,49 @@
-"use client"
-import { createContext, useState, useContext } from "react";
-import { data } from "@/data.json"
+"use client";
+import { createContext, useState, useContext, useEffect } from "react";
+import { data } from "@/data.json";
 
 const AppContext = createContext(data);
 
 export function AppWrapper({ children }) {
-  let [state, setState] = useState({data});
-  
-  return (<AppContext.Provider value={state}>{children}</AppContext.Provider>);
+  const [state, setState] = useState({
+    allProducts: [],
+    phones: [],
+    accessories: [],
+    tablets: [],
+    tvs: [],
+    hotToday:[],
+    mixDataCard:[]
+  });
+  useEffect(() => {
+    const allProducts = data.products;
+    const phone = data.products.filter((product) => product.type === "phone");
+    const accessories = data.products.filter(
+      (product) => product.type === "accessories"
+    );
+    const tablet = data.products.filter((product) => product.type === "tablet");
+    const tv = data.products.filter((product) => product.type === "tv");
+
+    const hotToday = [...phone.slice(0, 3), ...phone.slice(5, 6)];
+    const mixDataCard = [
+      ...accessories,
+      ...tablet.slice(3, 5),
+      ...phone.slice(7, 8),
+    ];
+
+    setState({
+      allProducts,
+      phone,
+      accessories,
+      tablet,
+      tv,
+      hotToday,
+      mixDataCard
+    });
+  }, []);
+
+  return <AppContext.Provider value={state}>{children}</AppContext.Provider>;
 }
 
 export function useAppContext() {
-    return useContext(AppContext)
+  return useContext(AppContext);
 }
