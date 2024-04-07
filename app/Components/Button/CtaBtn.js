791be4e4e62ed3485f25459449cf4e0ac13f0246
tmg-m <bus.mahendrat@gmail.com>
2024-04-07 04:24:24 +0200

    cta logic updated for handeling SingleProductCheckout

--- app/Components/Button/CtaBtn.js
+++ app/Components/Button/CtaBtn.js
@@ -1,3 +1,8 @@
+"use client";
+import { globalStore } from "@/app/store/store";
+import Link from "next/link";
+import { useState, useEffect } from "react";
+
 export default function CtaBtn({
   id,
   type,
@@ -6,34 +11,42 @@ export default function CtaBtn({
   confirmation,
   singleProduct,
 }) {
+  const updateSingleProductCheckout = globalStore(
+    (state) => state.updateSingleProductCheckout
+  );
+  const [url, setUrl] = useState("");
+
+  useEffect(() => {
+    if (isEnable) {
+      const params = { id, type };
+      const queryString = new URLSearchParams(params).toString();
+
+      let destinationUrl = "/checkout";
+      if (confirmation) {
+        destinationUrl += "/confirmation";
+      } else if (singleProduct) {
+        destinationUrl += `?${queryString}`;
+      } else {
+        destinationUrl += `?${queryString}`;
+      }
+
+      setUrl(destinationUrl);
+    }
+  }, [id, type, isEnable, confirmation, singleProduct]);
 
   const handleClick = () => {
-    const params = {
-      id: id,
-      type: type,
-    };
-    const queryString = new URLSearchParams(params).toString();
-    if (isEnable) {
-      if (confirmation) {
-        window.location.href = `/checkout/confirmation?${queryString}`;
-      } else if (singleProduct) {
-        window.location.href = `/checkout/?${queryString}&singleProduct=${singleProduct}`;
-      } else {
-        typeBtn == "checkout"
-          ? (window.location.href = `/checkout/?${queryString}`)
-          : console.log("here");
-      }
-    }
-  };
+    if (isEnable && singleProduct) {updateSingleProductCheckout(singleProduct);}
+  }
 
   return (
-    <div
-      className={`flex items-  justify-center px-8 py-3 text-white rounded-3xl cursor-pointer 
-      ${!isEnable ? "bg-red-300" : ""}
-      ${typeBtn === "checkout" ? "bg-red-600" : "bg-black"}`}
-      onClick={handleClick}
+    <Link
+      href={url}
+      className={`flex items-center justify-center px-8 py-3 text-white rounded-3xl cursor-pointer 
+    ${!isEnable ? "bg-red-300" : ""}
+    ${typeBtn === "checkout" ? "bg-red-600" : "bg-black"}`}
+    onClick={handleClick}
     >
       {typeBtn === "checkout" ? "Buy" : "Add to Cart"}
-    </div>
+    </Link>
   );
 }
