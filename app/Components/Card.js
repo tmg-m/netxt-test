db3abfdd6ca6abbc4dd079e5fdf64983a500833a
tmg-m <bus.mahendrat@gmail.com>
2024-04-09 23:30:26 +0200

    card update config for checkout

--- app/Components/Card.js
+++ app/Components/Card.js
@@ -1,31 +1,85 @@
+"use client";
 import Image from "next/image";
+import CheckCircleOutlineRoundedIcon from "@mui/icons-material/CheckCircleOutlineRounded";
 import ShoppingBagOutlinedIcon from "@mui/icons-material/ShoppingBagOutlined";
 import Link from "next/link";
+import { useAppContext } from "@/app/context/index";
+import { globalStore } from "@/app/store/store";
+import { useEffect, useState } from "react";
 
-export default function Card({ title, description, imageUrl, id, type, showAddToCart }) {
-  const handleClick = () => {
-    router.push(`/products/${type}/${id}`);
-  }
+export default function Card({
+  title,
+  description,
+  imageUrl,
+  id,
+  type,
+  showAddToCart,
+}) {
+  const { getProductById } = useAppContext();
+  const { updateProductCart, productCart } = globalStore((state) => state);
+
+  const [isInCart, setIsInCart] = useState(false);
+
+  useEffect(() => {
+    const productMatch = productCart.find((p) => p.id === id && true);
+    const isMatchFound = !!productMatch;
+    if (isMatchFound) {
+      setIsInCart(true);
+    }
+  }, [productCart, id, type]);
+
+  const handleClick = async () => {
+    const typeAndId = { productType: type, productId: id };
+    try {
+      const dataProduct = await getProductById(typeAndId);
+      const product = {
+        ...dataProduct,
+        storage_options_select: [dataProduct.storage_options[0]],
+      };
+      await updateProductCart(product);
+    } catch (error) {
+      console.error("Error fetching product:", error);
+    }
+  };
   return (
-    <Link href={`/products/${type}/${id}`} className="flex relative flex-col shadow-2xl rounded-xl bg-white cursor-pointer">
-      {showAddToCart && <div className="absolute top-2 md:top-4 right-2 md:right-4 rounded-full border p-1 md:p-2 flex items-center justify-center shadow-2xl bg-white">
-        <ShoppingBagOutlinedIcon className="text-gray-500" />
-      </div>}
-      <div className="flex flex-col justify-center items-center overflow-hidden p-5">
-        <Image
-          className="w-full h-full object-contain"
-          src={imageUrl}
-          alt="iphone 15 pro max"
-          height={1000}
-          width={1000}
-        />
-      </div>
-      {title && <>
-        <div className="flex justify-center items-center py-3 md:py-5 w-full border-t">
-          <p>{title}</p>
-        </div>  
-      </> 
-      }
-    </Link>
+    <div className="relative">
+      {showAddToCart && (
+        <>
+          {!isInCart ? (
+            <div
+              onClick={handleClick}
+              className="absolute top-2 md:top-4 right-2 md:right-4 rounded-full border p-1 md:p-2 flex items-center justify-center shadow-2xl bg-white"
+            >
+              <ShoppingBagOutlinedIcon className="text-gray-500" />
+            </div>
+          ) : (
+            <div className="absolute top-2 md:top-4 right-2 md:right-4 rounded-full border p-1 md:p-2 flex items-center justify-center shadow-2xl bg-white">
+              <CheckCircleOutlineRoundedIcon className="text-green-600" />
+            </div>
+          )}
+        </>
+      )}
+      <Link
+        href={`/products/${type}/${id}`}
+        className="flex flex-col shadow-2xl rounded-xl bg-white cursor-pointer"
+      >
+        <div className="flex flex-col justify-center items-center overflow-hidden p-5">
+          <Image
+            className="w-full h-full object-contain"
+            src={imageUrl}
+            alt="iphone 15 pro max"
+            height={1000}
+            width={1000}
+          />
+        </div>
+        {title && (
+          <>
+            <div className="flex justify-center items-center py-3 md:py-5 w-full border-t">
+              <p>{title}</p>
+            </div>
+          </>
+        )}
+      </Link>
+    </div>
   );
 }
