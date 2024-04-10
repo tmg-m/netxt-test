a6af566d484a21a7d785cabd392b1d75af2d9a4a
tmg-m <bus.mahendrat@gmail.com>
2024-04-10 03:32:20 +0200

    orders page reverse with css changes responsive

--- app/orders/page.js
+++ app/orders/page.js
@@ -9,7 +9,7 @@ export default function Orders() {
 
   useEffect(() => {
     if (purchaseOrders) {
-      setOrders(purchaseOrders);
+      setOrders(purchaseOrders.slice().reverse());
     }
   }, [purchaseOrders]);
 
@@ -20,28 +20,35 @@ export default function Orders() {
     .reduce((total, price) => total + price, 0);
 
   const handlePriceOrders = (products) => {
-    return products?.reduce((acc, product) => product.storage_options.map((option) => option.price).reduce((subtotal, price) => subtotal + price, 0), 0)
-  }
+    return products?.reduce(
+      (acc, product) =>
+        product.storage_options
+          .map((option) => option.price)
+          .reduce((subtotal, price) => subtotal + price, 0)
+          ?.toFixed(2),
+      0
+    );
+  };
 
   return (
     <div className="flex flex-col justify-center gap-5 mb-10 p-5 md:p-20">
       <p className="text-4xl">My orders</p>
       {orders &&
         orders.map((order, index) => (
-          <div key={index} className="flex flex-col gap-10 bg-ribbon p-10">
-            <p className="font-bold">
+          <div key={index} className="flex flex-col gap-5 md:gap-10 bg-ribbon p-10">
+            <p className="font-bold text-lg">
               Order number :{" "}
               <span className="font-normal">{order.orderNumber}</span>
             </p>
             {order.products && (
-              <div>
+              <div className="flex flex-col gap-8">
                 {order.products?.map((product) => (
                   <div
                     key={product.id}
                     className="flex flex-col md:flex-row justify-between items-center gap-10 pb-5 md:pb-10 border-b-2"
                   >
-                    <div className="flex justify-between items-center gap-5">
-                      <div className="max-w-[150px] max-h-[150px]">
+                    <div className="flex flex-col md:flex-row justify-between items-center gap-10">
+                      <div className="max-w-[150px] md:max-w-[200px] max-h-[200px] md:max-h-[300px]">
                         <Image
                           src={product.image_url.main}
                           className="object-contain bg-white rounded-3xl shadow-2xl w-full h-full"
@@ -50,20 +57,23 @@ export default function Orders() {
                           width={400}
                         />
                       </div>
-                      <div>
-                        <p>{product.title}</p>
+                      <div className="flex flex-col gap-3 w-full md:w-100">
+                        <p className="text-lg font-bold">{product.title}</p>
+                        <p>{product.storage_options_select[0].size}</p>
                       </div>
                     </div>
                     <p className="self-end">
-                      <span>Price: </span>
-                      {product.currency}
-                      {product.storage_options[0].price}
+                      <span className="text-xl">Price: </span>
+                      <span className="text-xl">
+                        {product.currency}
+                        {product.storage_options[0].price}
+                      </span>
                     </p>
                   </div>
                 ))}
               </div>
             )}
-            <div className="flex justify-center items-center gap-5 md:gap-10 md:self-end">
+            <div className="flex justify-end md:justify-center items-center gap-5 md:gap-10 md:self-end">
               {order.products && (
                 <div>
                   <p className="text-2xl mb-2">Total products</p>
