f610e276b8c657fda89f8ee7deb3853368ffa5b9
tmg-m <bus.mahendrat@gmail.com>
2024-04-12 21:08:02 +0200

    products page replace mock with data

--- app/products/page.js
+++ app/products/page.js
@@ -1,52 +1,50 @@
+"use client";
+import { useEffect, useState } from "react";
 import Card from "../Components/Card";
+import { useAppContext } from "@/app/context/index";
 
 export default function Product() {
-  const cardsData = {
-    phone: Array.from({ length: 5 }, (_, index) => ({
-      type: "phone",
-      id: index + 1,
-      imageUrl: "/iphone15test.png",
-      title: "iphone 15 pro max",
-      description: "This is iphone 15 pro max with amazing camera.",
-    })),
-    tablet: Array.from({ length: 3 }, (_, index) => ({
-      type: "tablet",
-      id: index + 1,
-      imageUrl: "/samsungTablet.png",
-      title: "Samsung Tablet",
-      description: "Samsung tablet with amazing features.",
-    })),
-    tv: Array.from({ length: 4 }, (_, index) => ({
-      type: "tv",
-      id: index + 1,
-      imageUrl: "/iphone15test.png",
-      title: "Samsung TV",
-      description: "Samsung TV with amazing display.",
-    })),
-    extra: Array.from({ length: 3 }, (_, index) => ({
-      type: "accessories",
-      id: index + 1,
-      imageUrl: "/samsungTablet.png",
-      title: "Airbuds",
-      description: "Samsung Airbuds with amazing sound quality.",
-    })),
-  };
+  const { getProduct } = useAppContext();
+  const [allProducts, setAllProducts] = useState();
+  const [productType, setProductType] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getProduct();
+        setAllProducts(data.products);
+        const types = ["phone", "tablet", "tv", "accessories"];
+        const filteredProducts = {};
+        types.forEach((type) => {
+          filteredProducts[type] = data.products.filter(
+            (product) => product.type === type
+          );
+        });
+        setProductType(filteredProducts);
+      } catch (error) {
+        console.error("Error fetching product:", error);
+      }
+    };
+    fetchData();
+  }, [getProduct]);
 
   return (
     <div className="flex flex-col">
       <div>filters functionality</div>
       <div className="felx flex-col bg-ribbon p-2 md:px-60">
-        {Object.keys(cardsData).map((type) => (
+        {Object.keys(productType).map((type) => (
           <div key={type} className="px-5 py-5 md:py-10">
-            <p className="border-b-2 text-lg font-medium mb-8 md:mb-10 pb-1">{type}</p>
+            <p className="border-b-2 text-lg font-medium mb-8 md:mb-10 pb-1">
+              {type}
+            </p>
             <div className="grid grid-cols-2 md:grid-cols-4 gap-5 md:gap-10 justify-center items-center mt-5">
-              {cardsData[type]?.map((card) => (
+              {productType[type]?.map((card) => (
                 <div key={card.id}>
                   <Card
                     key={card.id}
                     id={card.id}
                     type={card.type}
-                    imageUrl={card.imageUrl}
+                    imageUrl={`/iphone15test.png`}
                     title={card.title}
                     description={card.description}
                     showAddToCart
