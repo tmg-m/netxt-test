7496fdaae0b67be56b174dad192ff7c99b51d675
tmg-m <bus.mahendrat@gmail.com>
2024-04-15 20:32:26 +0200

    filter functionality handeling implementation

--- app/Components/Filter/Filter.js
+++ app/Components/Filter/Filter.js
@@ -1,8 +1,21 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
-export default function Filter({ productTypes }) {
+export default function Filter({ productTypes, onFilteredProducts }) {
   const [products, setProducts] = useState([]);
+  const lastFilteredProducts = useRef({});
+  const [checkedTypes, setCheckedTypes] = useState({
+    phone: false,
+    tablet: false,
+    tv: false,
+    accessories: false,
+  });
+  const [filteredProducts, setfilteredProducts] = useState({
+    phone: [],
+    tablet: [],
+    tv: [],
+    accessories: [],
+  });
 
   useEffect(() => {
     if (productTypes) {
@@ -10,34 +23,59 @@ export default function Filter({ productTypes }) {
     }
   }, [productTypes]);
 
-  const [checkedTypes, setCheckedTypes] = useState({
-    phone: false,
-    tablet: false,
-    tv: false,
-    accessories: false,
-  });
-
-  const handleCheckboxChange = (type, provider) => {
+  const handleCheckboxChange = (type, isChecked) => {
     setCheckedTypes((prevCheckedTypes) => ({
       ...prevCheckedTypes,
-      [provider]: !prevCheckedTypes[provider]
+      [type]: !prevCheckedTypes[type],
+    }));
+
+    setfilteredProducts((prevFilteredProducts) => ({
+      ...prevFilteredProducts,
+      [type]: isChecked ? products[type] : [],
     }));
   };
 
-  useEffect(() => {
-    if (checkedTypes) {
-      const filteredProducts = Object.keys(checkedTypes).reduce((acc, type) => {
-        if (checkedTypes[type]) {
-          return {
-            ...acc,
-            [type]: products[type]
-          };
-        }
-        return acc;
-      }, {});
-      console.log(filteredProducts);
+  const handleProviderCheckboxChange = (type, provider, isChecked) => {
+    if (products) {
+      const newCheckedTypes = { ...checkedTypes };
+
+      if (isChecked) {
+        newCheckedTypes[type] = Array.isArray(newCheckedTypes[type])
+          ? [...newCheckedTypes[type], provider]
+          : [provider];
+      } else {
+        newCheckedTypes[type] = Array.isArray(newCheckedTypes[type])
+          ? newCheckedTypes[type].filter((item) => item !== provider)
+          : false;
+      }
+      const filtered = products[type].filter((product) => {
+        const typeMatch = product.type === type;
+        const providerList = Array.isArray(newCheckedTypes[type])
+          ? newCheckedTypes[type]
+          : [];
+        const providerMatch = providerList.includes(product.provider);
+        return typeMatch && providerMatch;
+      });
+      setCheckedTypes(newCheckedTypes);
+      setfilteredProducts((prevFilteredProducts) => ({
+        ...prevFilteredProducts,
+        [type]: filtered.length > 0 ? filtered : products[type],
+      }));
     }
-  }, [checkedTypes, products]);
+  };
+
+  useEffect(() => {
+  const nonEmptyFilteredProducts = Object.fromEntries(
+    Object.entries(filteredProducts).filter(([type, products]) => products.length > 0)
+  );
+  const hasChanged = JSON.stringify(nonEmptyFilteredProducts) !== JSON.stringify(lastFilteredProducts.current);
+  if (hasChanged) {
+    const filteredData = Object.keys(nonEmptyFilteredProducts).length > 0 ? nonEmptyFilteredProducts : false;
+    onFilteredProducts(filteredData);
+    lastFilteredProducts.current = filteredData;
+  }
+}, [filteredProducts, onFilteredProducts]);
+
 
   return (
     <div className="flex flex-col items-start gap-8">
@@ -56,7 +94,7 @@ export default function Filter({ productTypes }) {
                   name={type}
                   value={type}
                   checked={checkedTypes[type]}
-                  onChange={() => handleCheckboxChange(type, type)}
+                  onChange={(e) => handleCheckboxChange(type, e.target.checked)}
                   className="w-5 h-5 rounded-full"
                 />
                 <p className="text-md font-bold">{type}</p>
@@ -69,6 +107,13 @@ export default function Filter({ productTypes }) {
                       name={type}
                       value={provider}
                       className="w-5 h-5 rounded-full"
+                      onChange={(e) =>
+                        handleProviderCheckboxChange(
+                          type,
+                          provider,
+                          e.target.checked
+                        )
+                      }
                     />
                     <p>{provider}</p>
                   </div>
