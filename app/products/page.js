2a762bff351b9b7fb9bfd3bb36304898621a1a80
tmg-m <bus.mahendrat@gmail.com>
2024-04-15 20:32:51 +0200

    working filter on page Products

--- app/products/page.js
+++ app/products/page.js
@@ -6,14 +6,13 @@ import Filter from "../Components/Filter/Filter";
 
 export default function Product() {
   const { getProduct } = useAppContext();
-  const [allProducts, setAllProducts] = useState();
   const [productType, setProductType] = useState({});
+  const [hasFilters, setHasFilters] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const data = await getProduct();
-        setAllProducts(data.products);
         const types = ["phone", "tablet", "tv", "accessories"];
         const filteredProducts = {};
         types.forEach((type) => {
@@ -28,17 +27,21 @@ export default function Product() {
     };
     fetchData();
   }, [getProduct]);
-
+  
+  const handleFilteredProducts = (filteredProducts) => {
+    setHasFilters(filteredProducts)
+  };
+  
   return (
     <div className="flex flex-col items-center justify-center bg-ribbon px-5 py-10">
-      <Filter productTypes={productType && productType} />
-      {Object.keys(productType).map((type) => (
+      <Filter productTypes={productType && productType} onFilteredProducts={handleFilteredProducts} />
+      {Object.keys(hasFilters ? hasFilters : productType).map((type) => (
         <div key={type}>
           <p className="border-b-2 text-lg font-medium mb-8 md:mb-10 w-full">
             {type}
           </p>
           <div className="grid grid-cols-2 md:grid-cols-4 gap-5 md:gap-10 justify-center items-center mb-10">
-            {productType[type]?.map((card) => (
+            {(hasFilters ? hasFilters[type] : productType[type])?.map((card) => (
               <Card
                 key={card.id}
                 id={card.id}
