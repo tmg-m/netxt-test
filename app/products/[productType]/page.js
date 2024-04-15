6910983c411ee36c210bcde542102e3adae8cef5
tmg-m <bus.mahendrat@gmail.com>
2024-04-15 22:04:38 +0200

    productType page filter intergation

--- app/products/[productType]/page.js
+++ app/products/[productType]/page.js
@@ -1,17 +1,21 @@
 "use client";
 import Card from "@/app/Components/Card";
+import Filter from "@/app/Components/Filter/Filter";
 import { useAppContext } from "@/app/context/index";
 import { useEffect, useState } from "react";
 
 export default function ProductType({ params }) {
   const { getProductByType } = useAppContext();
+  const [productType, setProductType] = useState(null);
   const [products, setProducts] = useState(null);
+  const [hasFilters, setHasFilters] = useState(false);
 
   useEffect(() => {
     const getData = async () => {
       try {
         const data = await getProductByType(params.productType);
-        setProducts(data);
+        setProducts({ [params.productType]: data });
+        setProductType(params.productType);
       } catch (error) {
         console.error("Error fetching product:", error);
       }
@@ -19,11 +23,27 @@ export default function ProductType({ params }) {
     getData();
   }, [getProductByType, params]);
 
+  const handleFilteredProducts = (filteredProducts) => {
+    setHasFilters(filteredProducts);
+  };
+
+  console.log(hasFilters)
+  console.log(products)
+
   return (
-    <div className="flex flex-col justify-center">
-      <div className="flex gap-4 justify-center items-center bg-ribbon">
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 md:gap-10 justify-center items-center px-5 py-10">
-          {products?.map((card) => (
+    <div className="flex justify-center bg-ribbon py-10 gap-10 px-5">
+      <div className="hidden md:block">
+        <Filter
+          productTypes={products && products}
+          onFilteredProducts={handleFilteredProducts}
+        />
+      </div>
+      <div className="flex flex-col gap-4 justify-center items-center">
+        <p className="border-b-2 text-lg font-medium mb-8 md:mb-10 w-full">
+          {productType}
+        </p>
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 md:gap-10 justify-center items-center">
+          {products && (hasFilters ? hasFilters[productType] : products[productType]).map((card) => (
             <Card
               key={card.id}
               id={card.id}
